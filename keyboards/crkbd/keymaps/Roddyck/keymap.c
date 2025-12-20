37a650cbbe1ab4106ed666b71dec521faa804c28
Roddyck <rodion.ivashchenko2003@gmail.com>
2025-12-20 16:47:00 +0300

    some more tweaks

--- keyboards/crkbd/keymaps/Roddyck/keymap.c
+++ keyboards/crkbd/keymaps/Roddyck/keymap.c
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, XXXXXXX,  XXXXXXX,   KC_MSTP, KC_MNXT, KC_MPLY
+                                          _______, XXXXXXX,  XXXXXXX,   KC_MSTP, KC_MPLY, KC_MUTE
                                       //`--------------------------'  `--------------------------'
   ),
 
